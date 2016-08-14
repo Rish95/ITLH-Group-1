0718f09ffeeea362de4ba16bd79e36deba258ded
PrateekBopaiah <prateekbopaiah621995@gmail.com>
2016-08-14 10:34:10 +0530

    2.2

--- application/views/user/forgotpassword.php
+++ application/views/user/forgotpassword.php
@@ -12,25 +12,24 @@
   			<div class="col-md-4">
             	<div class="new-car-con">
 
-<form method="post" action="doforget" role="form" >
-<div class="form-group">
-<?php if($this->session->flashdata('message')) {?>
- <label><span style="color: #CC6633"><?php echo $this->session->flashdata('message');?><span></label>
-<?php }?>
+					<form method="post" action="doforget" role="form" >
+						<div class="form-group">
+							<?php if($this->session->flashdata('message')) {?>
+ 									<label><span style="color: #CC6633"><?php echo $this->session->flashdata('message');?><span></label>
+							<?php }?>
 
-</div>
-<h4>Forget pasword</h4>
-<div class="form-group">
-    <label for="emailid">Email Id</label>
-     <input name="emailid" type="email" size="25" id="emailid" placeholder="Enter email" class="form-control" value="<?php echo set_value('emailid');?>" />
-     <span style="color:red"><?php echo form_error('emailid');?></span>
-     </div>
-      
-  
-  <button type="submit" class="btn btn-default">Submit</button>
-</form>
-</div></div>
-</div>
+						</div>
+						<h4>Forget pasword</h4>
+						<div class="form-group">
+    						<label for="mail">Email Id</label>
+   					    	<input name="mail" type="email" size="25" id="mail" placeholder="Enter email" class="form-control" value="<?php echo set_value('mail');?>" />
+     						<span style="color:red"><?php echo form_error('mail');?></span>
+     					</div>
+      				    <button type="submit" class="btn btn-default">Submit</button>
+					</form>
+				</div>
+			</div>
+		</div>
 </div>
 </body>
 </html>
